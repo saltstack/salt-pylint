b936a693ad07755902a665f407c7324d05fe590e
Pedro Algarvio <pedro@algarvio.me>
2015-03-31 12:41:39 +0100

    Bump version for release

--- saltpylint/version.py
+++ saltpylint/version.py
@@ -8,5 +8,5 @@
     :license: Apache 2.0, see LICENSE for more details.
 '''
 
-__version_info__ = (2015, 1, 25)
+__version_info__ = (2015, 3, 31)
 __version__ = '.'.join(map(str, __version_info__))
