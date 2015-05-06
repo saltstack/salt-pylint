ae2307a74957a24ff7fb48cab725d9114e8a3ce5
Pedro Algarvio <pedro@algarvio.me>
2015-05-06 13:11:58 +0100

    We now should also support Python >= 3.4

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
