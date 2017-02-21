94a47ba522a59727b2b31f3d1fab9ff4d1265c6b
Pedro Algarvio <pedro@algarvio.me>
2017-02-21 17:59:03 +0000

    Py3 compat

--- saltpylint/py3modernize/fixes/fix_imports_salt_six.py
+++ saltpylint/py3modernize/fixes/fix_imports_salt_six.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import absolute_import
+
+# Import 3rd-party libs
+import six
 from libmodernize.fixes import fix_imports_six as libmodernize_fix_imports
 from lib2to3.fixes import fix_imports as lib2to3_fix_imports
 
 MAPPING = {}
 
-for key, value in libmodernize_fix_imports.FixImportsSix.mapping.iteritems():
+for key, value in six.iteritems(libmodernize_fix_imports.FixImportsSix.mapping):
     MAPPING[key] = 'salt.ext.{0}'.format(value)
 
 
