923c29378e1f8b938d08658f9cf9d3759b3d2764
Sergey Kizunov <sergey.kizunov@ni.com>
2018-04-27 11:00:00 -0500

    Add Windows support to the file permissions check
    Signed-off-by: Sergey Kizunov <sergey.kizunov@ni.com>

--- saltpylint/fileperms.py
+++ saltpylint/fileperms.py
@@ -77,6 +77,16 @@ class FilePermsChecker(BaseChecker):
                 # The octal representation in python 3 has changed to 0o644 instead of 0644
                 if desired_perm[idx][1] != 'o':
                     desired_perm[idx] = '0o' + desired_perm[idx][1:]
+            if sys.platform.startswith('win'):
+                # Windows does not distinguish between user/group/other.
+                # They must all be the same. Also, Windows will automatically
+                # set the execution bit on files with a known extension
+                # (eg .exe, .bat, .com). So we cannot reliably test the
+                # execution bit on other files such as .py files.
+                user_perm_noexec = int(desired_perm[idx][-3])
+                if user_perm_noexec % 2 == 1:
+                    user_perm_noexec -= 1
+                desired_perm[idx] = desired_perm[idx][:-3] + (str(user_perm_noexec) * 3)
 
         module_perms = oct(stat.S_IMODE(os.stat(node.file).st_mode))
         if sys.version_info < (3,):
@@ -84,9 +94,25 @@ class FilePermsChecker(BaseChecker):
 
         if len(desired_perm) == 1:
             if module_perms != desired_perm[0]:
+                if sys.platform.startswith('win'):
+                    # Check the variant with execution bit set due to the
+                    # unreliability of checking the execution bit on Windows.
+                    user_perm_noexec = int(desired_perm[0][-3])
+                    desired_perm_exec = desired_perm[0][:-3] + (str(user_perm_noexec + 1) * 3)
+                    if module_perms == desired_perm_exec:
+                        return
                 self.add_message('E0599', line=1, args=(desired_perm[0], module_perms))
         else:
             if module_perms < desired_perm[0] or module_perms > desired_perm[1]:
+                if sys.platform.startswith('win'):
+                    # Check the variant with execution bit set due to the
+                    # unreliability of checking the execution bit on Windows.
+                    user_perm_noexec0 = int(desired_perm[0][-3])
+                    desired_perm_exec0 = desired_perm[0][:-3] + (str(user_perm_noexec0 + 1) * 3)
+                    user_perm_noexec1 = int(desired_perm[1][-3])
+                    desired_perm_exec1 = desired_perm[1][:-3] + (str(user_perm_noexec1 + 1) * 3)
+                    if desired_perm_exec0 <= module_perms <= desired_perm_exec1:
+                        return
                 desired_perm = '>= {0} OR <= {1}'.format(*desired_perm)
                 self.add_message('E0599', line=1, args=(desired_perm, module_perms))
 
