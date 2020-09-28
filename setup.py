16b75b7a10084ec50da8a1db73887e17de457673
Pedro Algarvio <pedro@algarvio.me>
2020-09-28 15:49:46 +0100

    Update supported python versions

--- setup.py
+++ setup.py
@@ -88,7 +88,10 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
