b461dce2184eff2178b09277a4a4f1a81a086989
Pedro Algarvio <pedro@algarvio.me>
2024-02-02 10:46:16 +0000

    Remove some old and no longer required linters

--- saltpylint/py3modernize/fixes/fix_filter_salt_six.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from libmodernize.fixes import fix_filter
-
-
-class FixFilterSaltSix(fix_filter.FixFilter):
-
-    skip_on = 'salt.ext.six.moves.filter'
