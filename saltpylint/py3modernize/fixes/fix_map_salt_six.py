025792bd6a4a1cdfc4c261ea1d0d484a31fdd9be
Pedro Algarvio <pedro@algarvio.me>
2024-02-02 10:46:16 +0000

    Remove some old and no longer required linters

--- saltpylint/py3modernize/fixes/fix_map_salt_six.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from libmodernize.fixes import fix_map
-
-
-class FixMapSaltSix(fix_map.FixMap):
-
-    skip_on = 'salt.ext.six.moves.map'
--- saltpylint/py3modernize/fixes/fix_xrange_salt_six.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import absolute_import
-from libmodernize.fixes import fix_xrange_six
-
-
-class FixXrangeSaltSix(fix_xrange_six.FixXrangeSix):
-
-    skip_on = 'salt.ext.six.moves.range'
